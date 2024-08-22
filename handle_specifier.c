928655edc3f9b2c90f77a670ba56db87adf0877f
Raphael Melnique <158978700+Raphaelnpl@users.noreply.github.com>
2024-08-22 14:13:29 +0000

    update

--- handle_specifier.c
+++ handle_specifier.c
@@ -6,24 +6,83 @@
  * handle_specifier - Handles a format specifier and prints the corresponding output.
  * @format: The format specifier.
  * @args: The list of arguments.
+ * 
  * Return: The number of characters printed.
  */
 int handle_specifier(const char *format, va_list args)
 {
 	int count = 0;
 
-	if (*format == 'c')
+	switch (*format)
 	{
-		char c = va_arg(args, int);
-		count += write(1, &c, 1);
+		case 'c':
+			{
+				char c = va_arg(args, int);
+				count += write(1, &c, 1);
+				break;
+			}
+		case 's':
+			{
+				char *str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					count += write(1, str++, 1);
+				}
+				break;
+			}
+		case '%':
+			{
+				count += write(1, "%", 1);
+				break;
+			}
+		case 'd':
+		case 'i':
+			{
+				int num = va_arg(args, int);
+				count += print_number(num);
+				break;
+			}
+		default:
+			{
+				count += write(1, "%", 1);
+				count += write(1, format, 1);
+				break;
+			}
 	}
-	else if (*format == 's')
+
+	return count;
+}
+
+/**
+ * print_number - Prints an integer number.
+ * @n: The integer to be printed.
+ * 
+ * Return: The number of characters printed.
+ */
+int print_number(int n)
+{
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
 	{
-		char *str = va_arg(args, char *);
-		while (*str)
-		{
-			count += write(1, str++, 1);
-		}
+		count += write(1, "-", 1);
+		num = -n;
 	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		count += print_number(num / 10);
+	}
+
+	char digit = (num % 10) + '0';
+	count += write(1, &digit, 1);
+
 	return count;
 }
